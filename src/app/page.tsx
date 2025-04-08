3cb5fe431c8736f1ffc697ad78d4b3df7f10210a
Liam <github@box1.me>
2025-04-08 11:30:38 -0600

    Updated Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           
           <div className="mb-16 flex justify-center">
             <Image
-              src="/KMWorkflow.png"
+              src="/KMWorkflow1.png"
               alt="RAG Process Diagram"
               width={560}
               height={280}
